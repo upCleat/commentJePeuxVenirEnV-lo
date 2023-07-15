794ecf0b5e63b801f3ba63ff823b27d37106df6a
TisseurDeToile <webmaster@tisseurdetoile.net>
2023-07-15 19:10:15 +0200

    feat(router): ajut du débranchement des docs

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DocsView from '../views/DocsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/docs',
+      name: 'docs',
+      component: DocsView
     },
     {
       path: '/about',
