0a26e1ed463b8fa75836fbf1271a6bfd5e1d8627
TisseurDeToile <webmaster@tisseurdetoile.net>
2023-07-16 10:57:05 +0200

    chore(vite.config): change la cible du build vers docs

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  build: {
+    outDir: './docs'
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
